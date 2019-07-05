37017b35ec61b757200404318063f0a4238a899d
Fauzi <fauzibinfaisal@gmail.com>
2019-07-05 15:04:00 +0700

    coba

--- IOSUserData/ViewController.swift
+++ IOSUserData/ViewController.swift
@@ -10,11 +10,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var wordTF: UITextField!
+    @IBOutlet weak var dataLabel: UILabel!
+    
+    //key bakalan disimpan ditempat yg sama
+    let userDef = UserDefaults.standard
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
+
+        dataLabel.text = userDef.string(forKey: "word")
+
         // Do any additional setup after loading the view.
     }
-
+    @IBAction func saveButton(_ sender: UIButton) {
+        
+        //Menambahkan key baru
+        userDef.set(wordTF.text, forKey: "word")
+        //Memanggil userDef yang tersimpan
+        dataLabel.text = userDef.string(forKey: "word")
+        
+        //Menampilkan lokasi userDef tersimpan
+        print(NSSearchPathForDirectoriesInDomains(.libraryDirectory, .userDomainMask, true))
+        
+        
+    }
+    
+    
+    
+    
 
 }
 
